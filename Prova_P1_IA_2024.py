eff3f265a818f39770d5c7379cba85cd98fe2b12
Leonardo Rocha <lrrocha2003@gmail.com>
2024-09-20 17:13:00 -0300

    Tentativa 2

--- Prova_P1_IA_2024.py
+++ Prova_P1_IA_2024.py
@@ -1,6 +1,9 @@
+# Importações para o codigo
 import tkinter as tk
 
 janela_Principal = tk.Tk()
 janela_Principal.title('Prova P1 | Inteligência Artificial | Eng. da Computação 2024')
+
+padding = {'padx:5'}
 
 janela_Principal.mainloop()
